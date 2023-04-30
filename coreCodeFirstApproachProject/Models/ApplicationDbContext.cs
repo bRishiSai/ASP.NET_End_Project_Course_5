b3e2b9fce7b3115dc358e940508603959f149764
bRishiSai <brishisai@teksystems.com>
2023-04-30 23:35:08 +0530

    first commit

---
+++ coreCodeFirstApproachProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace coreCodeFirstApproachProject.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }  
+        public DbSet<User1> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=HSC-PF25NJE3;Database=ProjectDB;Integrated Security=true;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ coreCodeFirstApproachProject/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace coreCodeFirstApproachProject.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public int AvailableQuantity { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
+        public DateTime ProductAddedDate { get; set; }
+
+        public Category Category { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
